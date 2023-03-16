0dcc52d5cec9c76d3e34cb1bb1d38abbb74a2b0c
Ritika <ritika.digra@beckeralliance.com>
2023-03-16 16:51:45 +0530

    code cleanup

--- app/src/main/java/com/example/mvcexample/MainActivity.kt
+++ app/src/main/java/com/example/mvcexample/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.mvcexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mvcexample.controller.ILoginController
 import com.example.mvcexample.controller.LoginController
 import com.example.mvcexample.view.ILoginView
